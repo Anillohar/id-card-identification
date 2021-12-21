d5fbb7d1611dedf495c39db7e0213f7712be51ab
Anillohar <Anillohar03@gmail.com>
2021-12-21 15:57:31 +0530

    Add setup file to make it a python module

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='id-card-identifier',
+    version='1.0',
+    author='Anil Lohar',
+    author_email='anillohar03@gmail.com',
+    description='',
+    long_description='',
+    package_dir={'': '.'},
+    packages=find_packages(where="."),
+)
